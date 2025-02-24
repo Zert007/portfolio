a6aa85bc0a6c8b27575d47abe64dd66dc0d709d7
zert007 <gielrossel@gmail.com>
2025-02-24 14:01:11 +0100

    remote restore

--- backend/models/project.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ProjectSchema = new mongoose.Schema({
-  Title: String,
-  beschrijving: String,
-  Tags: [String]
-});
-
-module.exports = mongoose.model("Project", ProjectSchema);
--- backend/models/skill.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const SkillSchema = new mongoose.Schema({
-  name: String,
-  skillLevel: Number,
-  category: String
-});
-
-module.exports = mongoose.model("Skill", SkillSchema);
